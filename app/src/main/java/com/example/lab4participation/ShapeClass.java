8d998b5d7abd9fc7e87ade063c1b6405f8423868
kashishm007 <50669884+kashishm007@users.noreply.github.com>
2022-09-27 18:14:55 -0600

    shape class: string color added

--- app/src/main/java/com/example/lab4participation/ShapeClass.java
+++ app/src/main/java/com/example/lab4participation/ShapeClass.java
@@ -3,6 +3,7 @@ package com.example.lab4participation;
 public abstract class ShapeClass {
     private int x;
     private int y;
+    private String color = "blue";
     public ShapeClass(int x, int y){
     }
 }
