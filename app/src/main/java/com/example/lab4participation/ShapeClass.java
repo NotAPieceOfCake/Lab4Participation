76606900d5a6e7a040015678eecbda46fee40175
Abhishek Solanki <asolanki@ualberta.ca>
2022-09-28 16:04:58 -0600

    updated shape class

--- app/src/main/java/com/example/lab4participation/ShapeClass.java
+++ app/src/main/java/com/example/lab4participation/ShapeClass.java
@@ -3,8 +3,8 @@ package com.example.lab4participation;
 public abstract class ShapeClass {
     private int x;
     private int y;
-    private String color;
-    color = "$PUT_YOUR_CHOICE_OF_COLOR";
+    private String color = "black";
+
     public ShapeClass(int x, int y){
     }
 }
