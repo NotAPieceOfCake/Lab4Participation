f3dd5139b7553977ad9348f0a2e329e1d0fb1f5e
Tanu Goyal <tanugoyal@Tanus-MacBook-Pro.local>
2022-09-27 18:13:57 -0600

    adding string color

--- app/src/main/java/com/example/lab4participation/ShapeClass.java
+++ app/src/main/java/com/example/lab4participation/ShapeClass.java
@@ -1,6 +1,7 @@
 package com.example.lab4participation;
 
 public abstract class ShapeClass {
+    private String color = "blue";
     private int x;
     private int y;
     public ShapeClass(int x, int y){
